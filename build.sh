e4418316b7518dcd7057f613b7341af98276c948
Andres Perez <aperez@salesforce.com>
2021-03-05 18:07:36 -0500

    Print messages on build script

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+echo "Make Read/Write"
 chmod -R 777 ./dist
 chmod -R 777 ./lib
+echo "Build..."
 yarn build:client 
 yarn build:server
+echo "Make Read Only"
 chmod -R 555 ./dist
 chmod -R 555 ./lib
